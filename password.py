4bbd138b37d1b76dc117a5c59bb002abad88a7f8
Stephen Yang <stefan8935@hotmail.com>
2021-03-24 10:12:11 +1100

    basic version final

--- password.py
+++ password.py
@@ -4,11 +4,15 @@
 password = 'a123456'
 n = 3 # maximum attempts
 while n > 0:
+	n = n - 1
 	pwd = input('Please enter your password.')
 	if pwd == password:
 		print('Welcome!')
 		break
 	else:
-		n = n - 1
-		print('Wrong password! You have', n,'attempts left!')
+		print('Wrong password!')
+		if n > 0:
+			print('You have', n,'attempts left!')
+		else:
+			print('No more attempts！')
 		
